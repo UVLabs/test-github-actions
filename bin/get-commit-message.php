418b06fd2c76ba3f7d42d6c62843b8a7dd5e7386
UVLabs <uriahs.victor@gmail.com>
2021-05-18 13:53:21 -0400

    testing

---
+++ bin/get-commit-message.php
@@ -0,0 +1,22 @@
+<?php
+
+$commit_message = getenv('COMMITTEXT');
+
+// $str = "Merge pull request #41 from UVLabs/master
+
+// test1
+// test2
+// test3";
+
+// $a = explode("\n", $str);
+$a = explode("\n", $commit_message);
+$newstr = '';
+
+foreach($a as $key => $value){
+    // Commit message always starts after 2 '\n\n' characters
+    if( $key >= 2 ){
+    $newstr .= '- ' . $value . PHP_EOL;
+    }
+}
+
+echo $newstr;
